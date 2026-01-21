6923f47ff14b37d556aefbde6237f12494a9a3b0
Puntokas <126613054+Puntokas@users.noreply.github.com>
2026-01-21 18:09:27 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors')
+const Fuse = require('fuse.js');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const db = mysql.createPool({
+    host: 'tramway.proxy.rlwy.net',     // or your MySQL server IP
+    port: '17541',
+    user: 'root',          // your MySQL username
+    password: 'tsfcyHKdvYkyVplEDiZTHRyhQzqyZpTK',          // your MySQL password
+    database: 'railway'      // your database name
+});
+
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error("Error connecting to MySQL:", err);
+    } else {
+        console.log("Connected to MySQL!");
+        connection.release(); // release connection back to pool
+    }
+});
+
+/*app.get('/list', (req, res) => {
+    const search = req.query.search || '';
+
+
+let sql = "SELECT * FROM items";
+    const params = [];
+    if (search) {
+        sql += " WHERE name LIKE ?";
+        params.push(`%${search}%`);
+    }
+
+    db.query(sql, params, (err, results) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(results);
+    });
+});*/
+
+app.get('/list', (req, res)=>{
+    const search = req.query.search || '';
+
+    db.query("SELECT * FROM items", (err, rows) => {
+        if(err) return res.status(500).json({error: err.message});
+
+        if(!search) return res.json(rows);
+
+        const fuse = new Fuse(rows, {
+            keys: ['item_name'],
+            threshold: 0.7,
+        });
+
+        const result = fuse.search(search).map(r=>r.item);
+        res.json(result);
+    })
+})
+
+app.listen(3001, () => {
+    console.log("Server running on port 3001");
+});
